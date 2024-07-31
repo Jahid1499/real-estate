311f35dac76d5e9095df8878de6028a2d1ab837b
jahid <jahid15-1499@diu.edu.bd>
2024-07-31 19:46:59 +0600

    Feat: create user schemas and model

---
+++ api/src/models/users.model.ts
@@ -0,0 +1,30 @@
+import { model, Schema } from 'mongoose';
+
+const userSchema = new Schema({
+    userName: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    name: {
+        type: String,
+        require: true,
+    },
+    email: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        require: true,
+    },
+    avatar: {
+        type: String,
+        default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+    },
+}, { timestamps: true });
+
+const User = model('User', userSchema);
+
+export default User;
