7bae3b7f3ec92ac6290621ed0f2dc01f51847851
jahid <jahid15-1499@diu.edu.bd>
2024-07-31 13:15:44 +0600

    Feat: create header component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
+import Header from "./components/Header"
 import About from "./pages/About"
 import Home from "./pages/Home"
 import NotFound from "./pages/NotFound"
@@ -11,6 +12,7 @@ const App = () => {
 
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<SignIn />} />
