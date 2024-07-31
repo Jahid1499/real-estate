d7c2dfdcd1cefb7c4c6e507a446ea10ba5cdf16a
jahid <jahid15-1499@diu.edu.bd>
2024-07-31 13:37:45 +0600

    Feat: project setup for api and create api for helth check

---
+++ api/src/index.ts
@@ -0,0 +1,33 @@
+import cors from "cors";
+import dotenv from 'dotenv';
+import express from "express";
+import morgan from "morgan";
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'))
+
+// health route
+app.get("/health", (_req, res) => {
+    res.status(200).json({ status: "Server up" })
+})
+
+// Routes
+
+
+// 404 error handler
+app.use((_req, res) => {
+    res.status(404).json({ message: "Not Found" });
+});
+
+// global error handler
+app.use((err, _req, res, _next) => {
+    console.error(err.stack);
+    res.status(500).json({ message: err.message });
+});
+
+const port = process.env.PORT || 4000;
+const service_name = process.env.SERVICE_NAME || 'Real estate'
+app.listen(port, () => console.log(`${service_name} is running on http://localhost:${port}`));
