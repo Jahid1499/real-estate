5d79d367cabc51d95e67bca6e62ef0b84abd85d7
jahid <jahid15-1499@diu.edu.bd>
2024-07-31 12:14:18 +0600

    install react js and tailwind css

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+
+const App = () => {
+
+  return (
+    <div>
+      <h1 className="text-3xl font-bold w-auto bg-red-400 text-red-600">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
